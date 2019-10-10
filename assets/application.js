413b0289f2f408e25bfe496ddfaaba95b9acd485
friederjanmoerman <hijbrid@gmail.com>
2019-10-10 13:24:07 +0200

    made the overlays work

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,34 @@
-// Put your applicaiton javascript here
+var menuToggle = document.querySelector('.body');
+
+var menuButtons = document.querySelectorAll('.js-toggle-menu');
+for (var i = 0; i < menuButtons.length; i++) {
+  menuButtons[i].addEventListener('click', function() {
+    if (menuToggle.classList.contains('open-menu')) {
+      menuToggle.classList.remove('open-menu');
+    } else {
+      menuToggle.classList.add('open-menu');
+    }
+  });
+}
+
+var advancedFilterButtons = document.querySelectorAll('.js-toggle-advanced-filter');
+for (var i = 0; i < advancedFilterButtons.length; i++) {
+  advancedFilterButtons[i].addEventListener('click', function() {
+    if (menuToggle.classList.contains('open-advanced-filter')) {
+      menuToggle.classList.remove('open-advanced-filter');
+    } else {
+      menuToggle.classList.add('open-advanced-filter');
+    }
+  });
+}
+
+var cartButtons = document.querySelectorAll('.js-toggle-cart-overlay');
+for (var i = 0; i < cartButtons.length; i++) {
+  cartButtons[i].addEventListener('click', function() {
+    if (menuToggle.classList.contains('open-cart')) {
+      menuToggle.classList.remove('open-cart');
+    } else {
+      menuToggle.classList.add('open-cart');
+    }
+  });
+}
