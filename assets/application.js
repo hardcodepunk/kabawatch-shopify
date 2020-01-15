fda62308ea0a8714e3cf57ebbdc7543c608a9106
friederjanmoerman <hijbrid@gmail.com>
2020-01-15 20:00:36 +0100

    fix some js issues, mainly the one about the fact some elements weren't found because they didn't exist on a certain page

--- assets/application.js
+++ assets/application.js
@@ -22,111 +22,116 @@ for (var i = 0; i < advancedFilterButtons.length; i++) {
   });
 }
 
-var shopPreviewImgs = document.querySelectorAll('.shop__list__item__visual');
-var shopListItem = document.querySelector('.shop__list__item');
-window.addEventListener("resize", function(){
-  shopListItemWidth = shopListItem.offsetHeight;
-  for (var i = 0; i < shopPreviewImgs.length; i++) {
-    shopPreviewImgs[i].style.height = shopListItemWidth;
-  }
-});
-
-var sliderItems = document.querySelectorAll('.shop-item__gallery__item');
-var sliderProjection = document.getElementById('slider-projection');
-for (var i = 0; i < sliderItems.length; i++) {
-  sliderItems[i].addEventListener('click', function() {
-    if (this.classList.contains('is-being-displayed')) {
-
-    } else {
-      for (var i = 0; i < sliderItems.length; i++) {
-        sliderItems[i].classList.remove('is-being-displayed');
-      }
-      this.classList.add('is-being-displayed');
-      sliderProjection.src = this.querySelector('.shop-item__gallery__item__visual__img').src;
-      var img = this.querySelector('.shop-item__gallery__item__visual__img');
-
-      displayedItem = document.querySelector('li.shop-item__gallery__item.is-being-displayed');
-      currentLocation = sliderItemsArray.indexOf(displayedItem) + 1;
-      paginationElementCurrent.innerHTML = currentLocation;
-
-      var previousItem = displayedItem.previousElementSibling;
-      var nextItem = displayedItem.nextElementSibling;
-
-      for (var i = 0; i < galleryBtns.length; i++) {
-        galleryBtns[i].classList.remove('is-hidden');
-      }
-
-      if ( nextItem == null ) {
-        btnNext.classList.add('is-hidden');
-      }
-
-      if (previousItem == null) {
-        btnPrevious.classList.add('is-hidden');
-      }
+if (document.querySelector('.shop__list__item') !== null) {
+  var shopPreviewImgs = document.querySelectorAll('.shop__list__item__visual');
+  var shopListItem = document.querySelector('.shop__list__item');
+  window.addEventListener("resize", function(){
+    shopListItemWidth = shopListItem.offsetHeight;
+    for (var i = 0; i < shopPreviewImgs.length; i++) {
+      shopPreviewImgs[i].style.height = shopListItemWidth;
     }
   });
 }
 
-var galleryBtns = document.querySelectorAll('.shop-item__gallery-controls__btn');
-var btnPrevious = document.getElementById('gallery__btn-previous');
-var btnNext = document.getElementById('gallery__btn-next');
+//gallery
+if (document.querySelector('.show') !== null) {
+  var sliderItems = document.querySelectorAll('.shop-item__gallery__item');
+  var sliderProjection = document.getElementById('slider-projection');
+  for (var i = 0; i < sliderItems.length; i++) {
+    sliderItems[i].addEventListener('click', function() {
+      if (this.classList.contains('is-being-displayed')) {
 
-var displayedItem = document.querySelector('li.shop-item__gallery__item.is-being-displayed');
-var sliderItemsArray = Array.prototype.slice.call(sliderItems);
-var currentLocation = sliderItemsArray.indexOf(displayedItem) + 1;
-var totalItems = sliderItems.length;
-var paginationElementTotal = document.getElementById('gallery__pagination__total');
-var paginationElementCurrent = document.getElementById('gallery__pagination__current');
+      } else {
+        for (var i = 0; i < sliderItems.length; i++) {
+          sliderItems[i].classList.remove('is-being-displayed');
+        }
+        this.classList.add('is-being-displayed');
+        sliderProjection.src = this.querySelector('.shop-item__gallery__item__visual__img').src;
+        var img = this.querySelector('.shop-item__gallery__item__visual__img');
 
-paginationElementCurrent.innerHTML = currentLocation;
-paginationElementTotal.innerHTML = totalItems;
+        displayedItem = document.querySelector('li.shop-item__gallery__item.is-being-displayed');
+        currentLocation = sliderItemsArray.indexOf(displayedItem) + 1;
+        paginationElementCurrent.innerHTML = currentLocation;
 
-for (var i = 0; i < galleryBtns.length; i++) {
+        var previousItem = displayedItem.previousElementSibling;
+        var nextItem = displayedItem.nextElementSibling;
 
-  galleryBtns[i].addEventListener('click', function() {
+        for (var i = 0; i < galleryBtns.length; i++) {
+          galleryBtns[i].classList.remove('is-hidden');
+        }
 
-    var displayedItem = document.querySelector('li.shop-item__gallery__item.is-being-displayed');
+        if ( nextItem == null ) {
+          btnNext.classList.add('is-hidden');
+        }
 
-    if (this == btnPrevious) {
-
-      var previousItem = displayedItem.previousElementSibling;
-
-      displayedItem.classList.remove('is-being-displayed');
-      previousItem.classList.add('is-being-displayed');
-
-      sliderProjection.src = previousItem.querySelector('.shop-item__gallery__item__visual__img').src;
-
-      if (previousItem.previousElementSibling == null) {
-        btnPrevious.classList.add('is-hidden');
+        if (previousItem == null) {
+          btnPrevious.classList.add('is-hidden');
+        }
       }
+    });
+  }
 
-      if (btnNext.classList.contains('is-hidden')) {
-        btnNext.classList.remove('is-hidden');
+  var galleryBtns = document.querySelectorAll('.shop-item__gallery-controls__btn');
+  var btnPrevious = document.getElementById('gallery__btn-previous');
+  var btnNext = document.getElementById('gallery__btn-next');
+
+  var displayedItem = document.querySelector('li.shop-item__gallery__item.is-being-displayed');
+  var sliderItemsArray = Array.prototype.slice.call(sliderItems);
+  var currentLocation = sliderItemsArray.indexOf(displayedItem) + 1;
+  var totalItems = sliderItems.length;
+  var paginationElementTotal = document.getElementById('gallery__pagination__total');
+  var paginationElementCurrent = document.getElementById('gallery__pagination__current');
+
+  paginationElementCurrent.innerHTML = currentLocation;
+  paginationElementTotal.innerHTML = totalItems;
+
+  for (var i = 0; i < galleryBtns.length; i++) {
+
+    galleryBtns[i].addEventListener('click', function() {
+
+      var displayedItem = document.querySelector('li.shop-item__gallery__item.is-being-displayed');
+
+      if (this == btnPrevious) {
+
+        var previousItem = displayedItem.previousElementSibling;
+
+        displayedItem.classList.remove('is-being-displayed');
+        previousItem.classList.add('is-being-displayed');
+
+        sliderProjection.src = previousItem.querySelector('.shop-item__gallery__item__visual__img').src;
+
+        if (previousItem.previousElementSibling == null) {
+          btnPrevious.classList.add('is-hidden');
+        }
+
+        if (btnNext.classList.contains('is-hidden')) {
+          btnNext.classList.remove('is-hidden');
+        }
+
+        currentLocation -= 1;
+        paginationElementCurrent.innerHTML = currentLocation;
+
+      } else if ( this == btnNext ) {
+
+        var nextItem = displayedItem.nextElementSibling;
+
+        displayedItem.classList.remove('is-being-displayed');
+        nextItem.classList.add('is-being-displayed');
+        sliderProjection.src = nextItem.querySelector('.shop-item__gallery__item__visual__img').src;
+
+        if ( nextItem.nextElementSibling == null ) {
+          btnNext.classList.add('is-hidden');
+        }
+
+        if (btnPrevious.classList.contains('is-hidden')) {
+          btnPrevious.classList.remove('is-hidden');
+        }
+
+        currentLocation += 1;
+        paginationElementCurrent.innerHTML = currentLocation;
       }
-
-      currentLocation -= 1;
-      paginationElementCurrent.innerHTML = currentLocation;
-
-    } else if ( this == btnNext ) {
-
-      var nextItem = displayedItem.nextElementSibling;
-
-      displayedItem.classList.remove('is-being-displayed');
-      nextItem.classList.add('is-being-displayed');
-      sliderProjection.src = nextItem.querySelector('.shop-item__gallery__item__visual__img').src;
-
-      if ( nextItem.nextElementSibling == null ) {
-        btnNext.classList.add('is-hidden');
-      }
-
-      if (btnPrevious.classList.contains('is-hidden')) {
-        btnPrevious.classList.remove('is-hidden');
-      }
-
-      currentLocation += 1;
-      paginationElementCurrent.innerHTML = currentLocation;
-    }
-  });
+    });
+  }
 }
 
 var filterAnchors = document.querySelectorAll('.js-filter a');
